da923be3d3a1576fc5653e0e12e414fbbb7d9f1e
NehalAs <nehalashraf104@gmail.com>
2024-08-27 16:28:05 +0300

    app styles

---
+++ lib/utils/app_ui.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class AppUI{
+
+  static const Color blue61 = Color(0xFF064061);
+  static const Color blueC8 = Color(0xFF208CC8);
+  static const Color blueEA = Color(0xFF45AFEA);
+  static const Color blueF2 = Color(0xFF4EB7F2);
+  static const Color blueFF = Color(0xFF98DAFF);
+  static const Color greyAA = Color(0xFFAAAAAA);
+  static const Color greyC4 = Color(0xFFC4C4C4);
+  static const Color greyE8 = Color(0xFFE8E8E8);
+  static const Color whiteF1 = Color(0xFFF1F1F1);
+  static const Color whiteFA = Color(0xFFFAFAFA);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color offWhiteCD = Color(0xFFE2DECD);
+  static const Color black32 = Color(0xFF292D32);
+  static const Color orange5E = Color(0xFFF3735E);
+  static const Color green7B = Color(0xFF7DD97B);
+}
---
+++ lib/utils/styles.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class Styles{
+
+  static TextStyle textStyle24w600() {
+    return const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle textStyle20w600() {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle textStyle20w500() {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle textStyle18w600() {
+    return const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+
+  static TextStyle textStyle16w700() {
+    return const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle textStyle16w600() {
+    return const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle textStyle16w500() {
+    return const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle textStyle16w400() {
+    return const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+
+  static TextStyle textStyle14w400() {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+
+  static TextStyle textStyle12w400() {
+    return const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+
+}
